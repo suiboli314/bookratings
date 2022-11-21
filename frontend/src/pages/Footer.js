2360ae2f667e5be4e52ff54b05d0173c887b033a
Qishu Dong <aidanseven@gmail.com>
2022-11-20 16:18:22 -0800

    Add footer

---
+++ frontend/src/pages/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Footer() {
+  return <div className="Footer">Made by Chen & Qishu with 😼</div>;
+}
+
+Footer.propTypes = {};
+
+export default Footer;
