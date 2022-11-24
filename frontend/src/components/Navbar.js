08100c7eb0dab262f172d9a97377de48b85a5694
suiboli314 <23085379+suiboli314@users.noreply.github.com>
2022-11-24 03:26:49 -0800

    fix footer covers contents

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = ({ auth }) => {
     <>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="relative flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
